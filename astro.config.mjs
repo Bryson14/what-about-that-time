40896117188f43c948bfed8a4f94b2800f6701d7
Bryson Meiling <bryson.meiling@gmail.com>
2026-05-16 14:12:49 -0700

    fixing edit pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,21 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  site: "https://timeline.smiling.dev",
+
   adapter: cloudflare({
     sessionKVBindingName: 'SESSION',
-  })
+  }),
+
+  integrations: [sitemap({
+    filter: (page) => {
+      // Exclude admin and login pages from sitemap
+      return !page.includes("/admin")
+    }
+  })]
 });
